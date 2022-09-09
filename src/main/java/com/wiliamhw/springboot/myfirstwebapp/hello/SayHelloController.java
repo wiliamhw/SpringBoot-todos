628d31b123b460c1792cfa5403b988e448f484ca
William H.W <wiliamwijaya1985@gmail.com>
2022-09-09 20:31:56 +0700

    feat: create say hello controller

---
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,20 @@
+package com.wiliamhw.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, world!";
+    }
+
+    @RequestMapping("/say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
+}
