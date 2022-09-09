df7a6117ff05cbf77e43dc10b0d5f879219fec23
William H.W <wiliamwijaya1985@gmail.com>
2022-09-09 20:47:59 +0700

    feat: handle request params and pass variabel to jsp

--- src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,19 @@
 package com.wiliamhw.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(
+            @RequestParam(required = false, defaultValue = "MyName") String name,
+            ModelMap model
+    ) {
+        model.put("name", name);
         return "login";
     }
 }
