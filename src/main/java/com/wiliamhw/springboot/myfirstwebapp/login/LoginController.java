f7dec3d9a318da26d2b3b70f15f149e4168c8713
William H.W <wiliamwijaya1985@gmail.com>
2022-09-12 00:01:43 +0700

    feat: add login page and welcome page

--- src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
@@ -3,17 +3,26 @@ package com.wiliamhw.springboot.myfirstwebapp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String gotoLoginPage(
-            @RequestParam(required = false, defaultValue = "MyName") String name,
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String gotoLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String gotoWelcomePage(
+            @RequestParam String name,
+            @RequestParam String password,
             ModelMap model
     ) {
         model.put("name", name);
-        return "login";
+        model.put("password", password);
+        
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <div>Welcome page</div>
+        <div>Your Name: ${name}</div>
+        <div>Your Password:${password}</div>
+    </body>
+</html>
