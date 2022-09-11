09af1ddccc262cd351071797c0e6a69d6f558fbe
William H.W <wiliamwijaya1985@gmail.com>
2022-09-11 23:14:06 +0700

    chore: add logging on login controller

--- src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.wiliamhw.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/login")
     public String gotoLoginPage(
             @RequestParam(required = false, defaultValue = "MyName") String name,
             ModelMap model
     ) {
         model.put("name", name);
+        logger.debug("Request param is {}", name);
+        logger.info("I want this printed at info level");
+        logger.warn("I want this printed at warn level");
+
+        System.out.println("Request param is " + name); // Not recommended for prod code
+
         return "login";
     }
 }
