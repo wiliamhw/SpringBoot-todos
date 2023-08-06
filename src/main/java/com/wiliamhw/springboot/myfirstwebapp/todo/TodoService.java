4d859aafb780e30a10c5c81df08fd3e5c4fa35ac
William H.W <wiliamwijaya1985@gmail.com>
2023-08-06 15:26:00 +0700

    feat: add todo

--- src/main/java/com/wiliamhw/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/todo/TodoService.java
@@ -11,16 +11,23 @@ public class TodoService {
 
     private static List<Todo> todos = new ArrayList<>();
 
+    private static int todosCount = 0;
+
     static {
-        todos.add(new Todo(1, "wiliamhw", "Learn AWS",
+        todos.add(new Todo(++todosCount, "wiliamhw", "Learn AWS",
                 LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(2, "wiliamhw", "Learn DevOps",
+        todos.add(new Todo(++todosCount, "wiliamhw", "Learn DevOps",
                 LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(3, "wiliamhw", "Learn Full Stack Development",
+        todos.add(new Todo(++todosCount, "wiliamhw", "Learn Full Stack Development",
                 LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean isDone) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, isDone);
+        todos.add(todo);
     }
 }
