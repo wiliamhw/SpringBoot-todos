810fbe67a3703e2b09da8370e35ab45cfc702aae
William H.W <wiliamwijaya1985@gmail.com>
2023-08-21 21:20:44 +0700

    feat: load initial sql data

--- src/main/java/com/wiliamhw/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/wiliamhw/springboot/myfirstwebapp/todo/Todo.java
@@ -1,18 +1,14 @@
 package com.wiliamhw.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
-
-    private int id;
-    private String username;
-
-    @Size(min = 10, message = "Enter at least 10 Characters...")
-    private String description;
-    private LocalDate targetDate;
-    private boolean done;
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
         this.id = id;
@@ -20,6 +16,20 @@ public class Todo {
         this.description = description;
         this.targetDate = targetDate;
         this.done = done;
+    }
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String username;
+
+    @Size(min = 10, message = "Enter at least 10 Characters...")
+    private String description;
+    private LocalDate targetDate;
+    private boolean done;
+
+    public Todo() {
     }
 
     public int getId() {
